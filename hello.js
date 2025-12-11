7aaaa442774e42180d1d38ada0629d774d764273
vanshika-CG <vanshika.jangamcg@gmail.com>
2025-12-11 15:28:50 +0530

    inbuilt modules

---
+++ hello.js
@@ -0,0 +1,7 @@
+const {add , subtract} = require('./index.js');
+
+const result = add(2,3);
+console.log(result);
+
+const result2 = subtract(5,2);
+console.log(result2);
---
+++ inbuilt.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// const result = fs.readFileSync('./index.js' , 'utf-8');
+// console.log(result);
+
+// const result = fs.writeFileSync('./index.js' , "hello")
+// console.log(result) // undefined;
+
+// fs.appendFileSync('./index.js' , "\n//hello world");
+
+// transfer one file content to anotherfile
+// fs.appendFileSync('./index.js' , fs.readFileSync('./hello.js' ))
+
+// console.log(fs);
